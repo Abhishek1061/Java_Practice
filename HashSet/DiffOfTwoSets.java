c3d5096290b53846b39e333a3de44b4f34219f5f
Abhishek Bhaskar Kadam <abhishekkadam1061@gmail.com>
2024-02-27 22:41:08 +0530

    Update DiffOfTwoSets.java

--- HashSet/DiffOfTwoSets.java
+++ HashSet/DiffOfTwoSets.java
@@ -1,7 +1,17 @@
 package HashSet;
 
-import java.util.HashSet; //imported HashSet from the java.util package
-import java.util.Set; //imported Set from the java.util package
+import java.util.HashSet;
+//imported HashSet from the java.util package
+//HashSet is a class that implements Set interface.
+//The storing of elements is done using hash table data structure.
+//It does not allow duplicate elements which are unordered, is also provide constant time performance for add, remove, contain and size operations.
+
+import java.util.Set;
+//imported Set interface from the java.util package where it represents a collection of unordered elements with no duplicates
+// it is subtype of  collection interface.
+//it provides methods for add, remove, contain, size and iteration operations.
+//Implementation of Set interface include HashSet, TreeSet, and LinkedHashSet
+
 
 public class DiffOfTwoSets {
     public static void main(String[] args) {
@@ -15,7 +25,7 @@ public class DiffOfTwoSets {
         numbers.add(3);
         numbers.add(4);
 
-        //Printing the content of numbers set
+        //Printing the content of numbers set in unordered manner.
         System.out.println("Numbers: " + numbers);
 
         // create second set
@@ -24,10 +34,10 @@ public class DiffOfTwoSets {
         //adding the prime integer values to set using add method form hashset package.
         primeNumbers.add(2);
         primeNumbers.add(3);
-        System.out.println("Prime Numbers: " + primeNumbers); //printing the primenumber set values.
+        System.out.println("Prime Numbers: " + primeNumbers); //printing the unordered "primeNumber" set values.
 
         // Difference between HashSet1 and HashSet2
-        numbers.removeAll(primeNumbers); //this will remove all the from the "number" set which are also present in "primeNumber " set.
-        System.out.println("Numbers without prime numbers: " + numbers); // this will give final list where there are no prime numbers in "number" set.
+        numbers.removeAll(primeNumbers); //this will remove all the elements from the "number" set which are also present in "primeNumber " set.
+        System.out.println("Numbers without prime numbers: " + numbers); // this will give final unordered list where there are no prime numbers in "number" set.
         }
 }
